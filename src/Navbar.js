771931fa1506277d8a7095ce844998a4dffe934e
sknabirasool <sknabirasool104@gmail.com>
2022-06-17 18:57:32 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>This is Data Blog</h1>
+            <div className="links">
+                <a href="/">Home
+                </a>
+                <a href="/create">Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
